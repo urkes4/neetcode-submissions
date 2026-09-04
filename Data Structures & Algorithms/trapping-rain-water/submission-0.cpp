d02525a23483eddeeb06f05bb7ecc3ca0fb6308a
urkes4 <165472757+urkes4@users.noreply.github.com>
2026-09-04 15:15:57 +0200

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l,r;
+        l = 0;
+        r = height.size()-1;
+        int leftMax = height[l];
+        int rightMax = height[r];
+        int res = 0;
+        while(l<r){
+            if(leftMax<rightMax){
+                l++;
+                leftMax = max(leftMax, height[l]);
+                res+=leftMax-height[l];
+            }
+            else{
+                r--;
+                rightMax = max(rightMax, height[r]);
+                res+=rightMax - height[r];
+            }
+        }
+        return res;
+    }
+};
